a1c51567369b2fbdebce59067276dc1a9f42f616
Denys209818 <deniskravcuk63@gmail.com>
2024-08-05 21:13:43 +0300

    Add image changes

--- DressShopBackend/DressShopBackend/Services/ImageHelper.cs
+++ DressShopBackend/DressShopBackend/Services/ImageHelper.cs
@@ -1,28 +1,45 @@
-﻿using System.Drawing;
+﻿
+
+using SkiaSharp;
 
 namespace DressShopBackend.Services
 {
     public static class ImageHelper
     {
-        public static Bitmap ConvertToBitmap(string base64)
+        public static void SaveImageFromBase64(string base64String, string outputPath)
         {
-            try
+            if (!string.IsNullOrEmpty(base64String))
             {
-                byte[] img = Convert.FromBase64String(base64);
-                using (MemoryStream ms = new MemoryStream(img))
+                //try
+                //{
+                byte[] imageBytes = Convert.FromBase64String(base64String);
+                using (var ms = new MemoryStream(imageBytes))
+                using (var bitmap = SKBitmap.Decode(ms))
+                using (var image = SKImage.FromBitmap(bitmap))
+                using (var data = image.Encode(SKEncodedImageFormat.Png, 100))
                 {
-                    ms.Position = 0;
-                    Image image = Image.FromStream(ms);
-                    ms.Close();
-                    img = null;
-                    return new Bitmap(image);
+                    using (var stream = File.OpenWrite(outputPath))
+                    {
+                        data.SaveTo(stream);
+                    }
                 }
-            }
-            catch
-            {
-                return null;
+                //    }
+                //    catch (FormatException ex)
+                //    {
+                //        Console.WriteLine("Invalid base64 string format: " + ex.Message + 
+                //            "\n" + "Data: " + ex.Data + "\nStackTrace: " + ex.StackTrace + "\nSource" + ex.Source);
+                //    }
+                //    catch (Exception ex)
+                //    {
+                //        Console.WriteLine("Error processing image: " + ex.Message +
+                //            "\n" + "Data: " + ex.Data + "\nStackTrace: " + ex.StackTrace + "\nSource" + ex.Source);
+                //    }
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Base64 string is null or empty.");
+                //}
             }
         }
-    
     }
 }
